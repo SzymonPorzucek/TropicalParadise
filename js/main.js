2a95a51a88f33de8534965363ea2c3ca0faf1ada
Szymon <szymoon.porzucek@gmail.com>
2024-04-05 21:16:57 +0200

    done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 const nav = document.querySelector(".nav");
 const navBtn = document.querySelector(".burger-btn");
 const allNavItems = document.querySelectorAll(".nav__item");
+const navBtnBars = document.querySelector(".burger-btn__bars");
+const allSections = document.querySelectorAll(".section");
+const footerYear = document.querySelector(".footer__year");
 
 const handleNav = () => {
+	navBtnBars.classList.remove('black-bars-color')
+	
 	nav.classList.toggle("nav--active");
 	allNavItems.forEach(item => {
 		item.addEventListener("click", () => {
@@ -20,4 +25,19 @@ const handleNavItemsNavigation = () => {
 		delayTime++;
 	});
 };
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+const handleObserver = () => {
+	const currentSection = window.scrollY;
+	allSections.forEach(section=>{
+		if(section.classList.contains('white-section') && section.offsetTop<=currentSection+60){
+			navBtnBars.classList.add('black-bars-color')
+		}else if(!section.classList.contains('white-section') && section.offsetTop<=currentSection+60){navBtnBars.classList.remove('black-bars-color')
+	}
+	})
+};
+handleCurrentYear();
 navBtn.addEventListener("click", handleNav);
+window.addEventListener('scroll', handleObserver)
